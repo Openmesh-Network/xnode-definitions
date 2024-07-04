61f8babefe74fb33226bbd1b033c4983a1cd66dc
harrys522 <hsackman@protonmail.com>
2024-07-04 11:00:04 +1000

    Change fields to add flags inidicating manually reviewed as working in UI and XnodeOS

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def main():
                 scraper_service = services_with_metadata[service]
                 new_service = {}
                 new_service = scraper_service
+                accum += 1
                 for svc in nix_option_data:
                     if svc['nixName'] == scraper_service['nixName']:
                         new_service['options'] = svc['options']
+                        new_service['show'] = False # show: service and options have been checked and are supported in the frontend
+                        new_service['tested'] = False # tested: service and options have been tested and are supported on Xnode
+                        new_service.pop('implmented', None) # Removed old, mispelt field
                         new_services.append(new_service)
-                        accum += 1
+                        
 
             print("Total services:",accum)
             print("Total potential services:", len(nix_option_data))
